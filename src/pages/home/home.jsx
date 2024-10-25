ab447f4183e8caa28dfa815dbaad819a717df1ab
jluchett <jluchett@hotmail.com>
2024-10-24 21:37:51 -0500

    Eliminado componente no HeroSection

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,15 +1,12 @@
 //import './home.css'
 
-import HeroSection from "../../components/heroSection/heroSection";
-import HeroSection2 from "../../components/carousel/carousel";
+import HeroSection from "../../components/carousel/carousel";
 
 const Home = () => {
   return (
     <>
       <div className="home-container">
-        <h1>Pagina Home</h1>
         <HeroSection />
-        <HeroSection2 />
       </div>
     </>
   )
